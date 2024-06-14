c2a62b14c13ab213b2fb0a3f4d1f0149d3d9ee4a
PURNA SATYA SAI LOVA KUMAR VASAMSETTY <135409164+v-sai-kumar@users.noreply.github.com>
2024-06-14 12:54:38 +0530

    Update mvnjenkins.sh

--- mvnjenkins.sh
+++ mvnjenkins.sh
@@ -24,7 +24,9 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/
 
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 
-yum install jenkins -y 
+yum install jenkins -y
+
+systemctl enable jenkins
 
 jenkins --version
 
